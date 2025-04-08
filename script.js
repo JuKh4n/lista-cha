8a21c0edd4860b430f80ccb98ad35e5a168c51fa
Juliano Souza <168299643+JuKh4n@users.noreply.github.com>
2025-04-08 02:29:02 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,59 @@
 const itens = [
-  "Jogo de pratos",
+  "Batedeira",
   "Liquidificador",
-  "Toalhas de banho",
-  "Jogo de panelas",
-  "Conjunto de copos"
+  "Cafeteira",
+  "Torradeira",
+  "Jogo de lençóis",
+  "Jogo de lençóis",
+  "Travesseiros",
+  "Jogo de panela",
+  "Fronhas avulsas",
+  "Jogo de jantar",
+  "Jogo de facas",
+  "Jogo de talheres",
+  "Varal de chão",
+  "Jogo de toalhas",
+  "Microondas",
+  "Forno eletrônico",
+  "Cesto de roupa",
+  "Assadeiras (vidro)",
+  "Edredom",
+  "Cobre leito",
+  "Potes herméticos (vidro)",
+  "Jogo de taças",
+  "Petisqueira",
+  "Potes para mantimentos",
+  "Galheteiros",
+  "Açucareiro",
+  "Saleiro",
+  "Jogo de sobremesa",
+  "Lixeira (cozinha)",
+  "Escorredor de louça",
+  "Ferro de passar roupas",
+  "Chaleira",
+  "Jarra elétrica",
+  "Panos de copa",
+  "Porta frios",
+  "Ralador",
+  "Porta temperos",
+  "Mixer",
+  "Tigelas",
+  "Tapete bolinha para banheiro",
+  "Ventilador",
+  "Tábua de cortes",
+  "Boleira",
+  "Escorredor de macarrão",
+  "Manteigueira",
+  "Organizador de salada",
+  "Garrafa térmica",
+  "Forma de alumínio",
+  "Jogo americano",
+  "Tábua de passar roupa",
+  "Mop",
+  "Cuia",
+  "Descanso de panelas",
+  "Triturador",
+  "Organizador de talheres"
 ];
 
 const lista = document.getElementById("lista-presentes");
@@ -19,12 +69,10 @@ function criarItem(nome, index) {
   lista.appendChild(li);
 }
 
-// Inicializar a lista
 itens.forEach((item, index) => {
   criarItem(item, index);
 });
 
-// Firebase (exemplo simples)
 const database = firebase.database();
 const reservasRef = database.ref("reservas");
 
@@ -36,6 +84,5 @@ function reservarItem(index) {
   }
 
   reservasRef.child(index).set(nome);
-
   document.getElementById(`reservado-${index}`).innerText = `Reservado por: ${nome}`;
 }
